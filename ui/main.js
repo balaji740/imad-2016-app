1e784a732305eacff21d9960ec3f072c866f8367
balaji740 <balaji740@gmail.com>
2016-09-24 19:48:24 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,38 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+button.onclick = function (){
+    var request = new XmlHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == XmlHttpRequest.DONE) {
+            if (request.status == 200) {
+                var counter = request.response.Text;
+                var span = document.getElementById('count');
+                span.innerHtml = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://balaji740.imad.hasura-app.io/counter', true);
+    request.send(null);
+}
+    
+//console.log('Loaded!');
 //change the text of main text-div
-var element = document.getElementById('main-text');
-element.innerHTML = 'new-value';
+//var element = document.getElementById('main-text');
+//element.innerHTML = 'new-value';
 
-var img = document.getElementById('madi');
+//var img = document.getElementById('madi');
 //img.onclick = function()
 //{
 //    img.style.marginLeft = '100px';
 //}
 //img.onClick = function()
-var marginLeft = 0;
-function moveRight()
-{
-   marginLeft = marginLeft + 1;
-   img.style.marginLeft = marginLeft + 'px';
-}
-   img.onclick = function(){
+//var marginLeft = 0;
+//function moveRight()
+//{
+//   marginLeft = marginLeft + 1;
+//   img.style.marginLeft = marginLeft + 'px';
+//}
+//   img.onclick = function(){
  
- var interval = setInterval(moveRight, 50);
- //img.style.marginLeft = '100px';
-};
+//var interval = setInterval(moveRight, 50);
+//img.style.marginLeft = '100px';
+//};
