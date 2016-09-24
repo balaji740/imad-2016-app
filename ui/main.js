dd7371d53f0b2f21b5f5abb2f8dbd579d9b4f9b8
balaji740 <balaji740@gmail.com>
2016-09-24 11:41:19 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'new-value';
 
 var img = document.getElementById('madi');
-img.onClick = function()
+img.onclick = function()
 {
     img.style.marginLeft = '100px';
 }
