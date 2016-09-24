de820fa3c9250b6971ef0fdf4e08646ac575f5a2
balaji740 <balaji740@gmail.com>
2016-09-24 11:52:44 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,19 +4,19 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'new-value';
 
 var img = document.getElementById('madi');
-img.onclick = function()
-{
-    img.style.marginLeft = '100px';
-}
-//img.onClick = function()
-//var marginLeft = 0;
-//function moveRight()
+//img.onclick = function()
 //{
- //   marginLeft = marginLeft + 1;
-//    img.style.marginLeft = marginLeft + 'px';
+//    img.style.marginLeft = '100px';
 //}
-//   img.onClick = function(){
-// 
-// var interval = setInterval(moveRight, 50);
+//img.onClick = function()
+var marginLeft = 0;
+function moveRight()
+{
+   marginLeft = marginLeft + 1;
+   img.style.marginLeft = marginLeft + 'px';
+}
+   img.onclick = function(){
+ 
+ var interval = setInterval(moveRight, 50);
  //img.style.marginLeft = '100px';
-//};
+};
