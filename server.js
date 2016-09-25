366e58150b5b8b31a69da62bfce2e11cd55712f9
balaji740 <balaji740@gmail.com>
2016-09-25 22:23:17 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/submit-name',function(req,res){
 });
 
 app.get('/:articleName', function (req, res) {
-  var articleName = req.params.articleName    
+  var articleName = req.params.articleName ;   
   res.send(createTemplate(articles[articleName]));
 });
 
