b3be2f474cdbcab427e94af739416c766424e691
balaji740 <balaji740@gmail.com>
2016-10-23 20:03:02 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+var config = {
+    user: 'balaji740',
+    database: 'balaji740',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var articles = {
     'article-one': {
     title: 'Article One Balaji Seshadri',
@@ -90,6 +98,17 @@ app.get('/counter',function(req, res) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+   pool.query('SELECT * FROM test',function(err,result){
+       if(err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+   }); 
+  
 });
 var names = [];
 app.get('/submit-name',function(req,res){
