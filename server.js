f8599f5c4f8f4abb268201064a7f78f6f6d7013a
balaji740 <balaji740@gmail.com>
2016-10-24 20:19:54 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,6 +136,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/favicon.ico', function(req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
 
 //app.get('/submit-name/:name', function (req, res) {
 
