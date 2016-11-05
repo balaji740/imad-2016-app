6455f06bd402c818fe39922d7fb92088cbf36597
balaji740 <balaji740@gmail.com>
2016-11-05 23:52:07 +1100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,18 @@
-var button = document.getElementById('counter');
-button.onclick = function (){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState == XMLHttpRequest.DONE) {
-            if (request.status == 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET','http://balaji740.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
+//var button = document.getElementById('counter');
+//button.onclick = function (){
+//    var request = new XMLHttpRequest();
+//    request.onreadystatechange = function () {
+//        if (request.readyState == XMLHttpRequest.DONE) {
+//            if (request.status == 200) {
+//                var counter = request.responseText;
+//                var span = document.getElementById('count');
+//                span.innerHTML = counter.toString();
+//            }
+//        }
+//    };
+//    request.open('GET','http://balaji740.imad.hasura-app.io/counter', true);
+//    request.send(null);
+//};
     
 //console.log('Loaded!');
 //change the text of main text-div
