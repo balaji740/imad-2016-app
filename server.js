a65c6dce8170d9268809416f71c152bcde960e88
balaji740 <balaji740@gmail.com>
2016-11-06 20:59:23 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,16 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+   
+}
+app.get('/hash/:input', function(req,res){
+    var hashedString = hash(req.params.input,'random-string');
+    res.send(hashedString);
+    });
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
    pool.query('SELECT * FROM test',function(err,result){
