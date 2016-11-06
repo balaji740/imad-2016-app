25bc89a7946cfc40c7d13418090e25754601bb34
balaji740 <balaji740@gmail.com>
2016-11-06 21:24:44 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 var Pool = require('pg').Pool;
 var config = {
     user: 'balaji740',
@@ -90,6 +91,7 @@ return htmlTemplate;
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {
@@ -105,6 +107,23 @@ app.get('/hash/:input', function(req,res){
     var hashedString = hash(req.params.input,'random-string');
     res.send(hashedString);
     });
+    
+app.post('/create-user', function(req,res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbstring = hash(password, salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1,$2)' , [username,dbstring], function(err, result) {
+        
+  if(err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send('User successfully created ' + username);
+       }
+   }); 
+  
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
    pool.query('SELECT * FROM test',function(err,result){
