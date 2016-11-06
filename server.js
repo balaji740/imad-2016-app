c27013fec1b09d8f232e025a058e47954e7f4c63
balaji740 <balaji740@gmail.com>
2016-11-06 18:54:43 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/submit-name',function(req,res){
 });
 
   //app.get('/:articleName', function (req, res) {
-  app.get('articles/:articleName', function (req, res) {
+  app.get('/articles/:articleName', function (req, res) {
   
   pool.query("SELECT * FROM article WHERE TITLE ='" + req.params.articleName + "'", function(err,result){
      if(err) {
