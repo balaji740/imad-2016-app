0f473431d50371609b72a536bfd40004d0bdc125
balaji740 <balaji740@gmail.com>
2016-11-06 18:53:09 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,9 +119,31 @@ app.get('/submit-name',function(req,res){
   res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
-  var articleName = req.params.articleName ;   
-  res.send(createTemplate(articles[articleName]));
+  //app.get('/:articleName', function (req, res) {
+  app.get('articles/:articleName', function (req, res) {
+  
+  pool.query("SELECT * FROM article WHERE TITLE ='" + req.params.articleName + "'", function(err,result){
+     if(err) {
+           res.status(500).send(err.toString());
+       }
+       else  {
+          if (result.rows.length === 0) {
+              res.status(400).send('article not found');
+          }
+        else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+       }
+       
+//     {
+//         res.send(JSON.stringify(result.rows));
+//     }
+//  
+//}
+  //var articleName = req.params.articleName ;   
+//res.send(createTemplate(articles[articleName]));
+});
 });
 
 app.get('/ui/style.css', function (req, res) {
