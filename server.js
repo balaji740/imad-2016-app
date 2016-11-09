b89db612f4b1143e5fd317dc240a75da3ea0bad2
balaji740 <balaji740@gmail.com>
2016-11-09 22:46:36 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.post('/login', function(req,res) {
               var salt = dbString.split('$')[2];
               var hashedPassword = hash(password,salt);
               if (hashedPassword === dbString) {
-                  req.session.auth = {userid: result.rows[0].id};
+                  req.session.auth = {userId: result.rows[0].id};
               res.send('credentials are correct');
              } else {
               
